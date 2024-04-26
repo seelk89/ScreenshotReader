76028712bba847e5ffe0773a5399cacceadf74e1
Seelk89 <seelk.89@gmail.com>
2024-04-26 16:04:12 +0200

    Removed unnecessary comment.

--- Misc/stringWatcherAndReader.py
+++ Misc/stringWatcherAndReader.py
@@ -13,21 +13,15 @@ class StringWatcherAndReader(threading.Thread):
 
     def run(self):
         while not self.stop_event.is_set():
-            # Get the value from the queue
             value = self.queue.get()
 
-            # Check if the value is not None and different from the previous value
             if value is not None and value != self.previous_value:
                 self.previous_value = value
-                # Check if the value is a string
                 if isinstance(value, str):
-                    # Perform the desired action, e.g., string_to_audio
                     string_to_audio(value)
-            # Sleep for some time before checking the queue again
-            time.sleep(1)
+            time.sleep(1)  # Sleep for performance.
 
     def stop(self):
-        # Set the stop event to stop the thread
         self.stop_event.set()
 
 
--- TTS/pyttsx3TTS.py
+++ TTS/pyttsx3TTS.py
@@ -1,13 +1,14 @@
 import pyttsx3
 
-class Pyttsx3TTS():
+
+class Pyttsx3TTS:
     def get_tts_audio(self, text):
         engine = pyttsx3.init()
 
-        engine.setProperty('rate', 150) # Speed percent (can go over 100)
-        engine.setProperty('volume', 0.8) # Volume 0-1
+        engine.setProperty('rate', 150)  # Speed percent (can go over 100).
+        engine.setProperty('volume', 0.8)  # Volume 0-1.
         voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[0].id) # 0 = female voice, 1 = male voice
+        engine.setProperty('voice', voices[0].id)  # 0 = female voice, 1 = male voice.
 
         engine.say(text)
         engine.runAndWait()
